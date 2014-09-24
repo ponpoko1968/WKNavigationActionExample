54104122d3ea41a7f6c504ded2265de97161069c
越智 修司 <ponpoko1968@gmail.com>
2014-09-24 16:50:03 +0900

    Initial commit.

--- WKNavigationActionExample/ViewController.h
+++ WKNavigationActionExample/ViewController.h
@@ -5,10 +5,10 @@
 //  Created by 越智 修司 on 2014/09/24.
 //  Copyright (c) 2014年 ClipReader project. All rights reserved.
 //
-
+#import <WebKit/WebKit.h>
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<WKNavigationDelegate>
 
 
 @end
--- WKNavigationActionExample/ViewController.m
+++ WKNavigationActionExample/ViewController.m
@@ -9,19 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic,strong) WKWebView *webView;
 
 @end
 
 @implementation ViewController
+-(void)loadView
+{
+  [super loadView];
+
+  self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+  [self.view insertSubview:self.webView atIndex:0];
+  self.webView.navigationDelegate = self;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  NSURLRequest* req = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com/"]];
+                       
+  [self.webView loadRequest:req];
+  
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+}
+
+- (void)webView:(WKWebView *)webView
+decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction
+decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+  Log1(navigationAction);
+  if([navigationAction.request.URL.host isEqualToString:@"www.apple.com"]){
+    decisionHandler(WKNavigationActionPolicyAllow);
+  }else{
+    decisionHandler(WKNavigationActionPolicyCancel);
+  }
 }
 
 @end
